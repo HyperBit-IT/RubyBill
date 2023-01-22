3de03fea803ab8f4d809dd22395f3b957841a2d5
Francesco Masala <mail@francescomasala.me>
2023-01-22 19:18:01 +0100

    Fixed Config - User Tenants

--- lib/rubybill/cf_user_tenants.rb
+++ lib/rubybill/cf_user_tenants.rb
@@ -5,26 +5,37 @@ module RubyBill
   module CFUserTenants
     include http
 
-    def sb_headers
-      HTTP.headers(
-        "Authorization" => "Bearer #{Ruby_bill.bearer}",
-        "Authorization-Key" => Ruby_bill.tenant_id.to_s
-      )
-    end
-
     def list_tenants
-      response = HTTP.get("https://superbill.datev.it/v10/user-tenants", headers: sb_headers)
-      response.parse.to_s
+      url = URI("https://superbill.datev.it/v10/user-tenants")
+      https = Net::HTTP.new(url.host.to_s, url.port)
+      https.use_ssl = true
+      request = Net::HTTP::Get.new(url)
+      request.content_type = "application/json"
+      request["Authorization"] = "Bearer #{RubyBill.bearer}"
+      request["Authorization-Key"] = RubyBill.tenant_id
+      JSON.parse(https.request(request).body)
     end
 
     def list_tenants_by_id(user_id)
-      response = HTTP.get("https://superbill.datev.it/v10/user-tenants/#{user_id}", headers: sb_headers)
-      response.parse.to_s
+      url = URI("https://superbill.datev.it/v10/user-tenants/#{user_id}")
+      https = Net::HTTP.new(url.host.to_s, url.port)
+      https.use_ssl = true
+      request = Net::HTTP::Get.new(url)
+      request.content_type = "application/json"
+      request["Authorization"] = "Bearer #{RubyBill.bearer}"
+      request["Authorization-Key"] = RubyBill.tenant_id
+      JSON.parse(https.request(request).body)
     end
 
     def list_active_tenants
-      response = HTTP.get("https://superbill.datev.it/v10/user-tenants/active", headers: sb_headers)
-      response.parse.to_s
+      url = URI("https://superbill.datev.it/v10/user-tenants/active")
+      https = Net::HTTP.new(url.host.to_s, url.port)
+      https.use_ssl = true
+      request = Net::HTTP::Get.new(url)
+      request.content_type = "application/json"
+      request["Authorization"] = "Bearer #{RubyBill.bearer}"
+      request["Authorization-Key"] = RubyBill.tenant_id
+      JSON.parse(https.request(request).body)
     end
   end
 end
