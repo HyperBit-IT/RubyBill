327662c7631243955daea9e9ddd26cfc4d37eef4
Francesco Masala <mail@francescomasala.me>
2023-01-27 17:41:29 +0100

    Defined the Config Error Structure

--- lib/rubybill/models_cf.rb
+++ lib/rubybill/models_cf.rb
@@ -3,6 +3,6 @@
 module RubyBill
   module ModelsCF
     # This class is used to represent an error returned by the SuperBill API.
-    SBApiError = Struct.new(:code, :message)
+    def SBApiError = Struct.new(:code, :message)
   end
 end
