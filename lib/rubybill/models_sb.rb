182975cd68272e1826e8753e7c25655527391a65
Francesco Masala <mail@francescomasala.me>
2023-01-27 17:40:13 +0100

    Added the Data Structure for SuperBill Article JSON file

--- lib/rubybill/models_sb.rb
+++ lib/rubybill/models_sb.rb
@@ -2,5 +2,10 @@
 
 module RubyBill
   module ModelsSB
+    # Data structures for SBArticles
+    def SBArticleData = Struct.new(:Codice, :Descrizione, :AltraDescrizione, :TipoArticolo,
+                                   :TipoSpesa, :TipoCalcoloSpesa, :TipoCalcoloPercentualeSpesa,
+                                   :CalcoloSpesaPercentuale, :IDUnitaMisura, :IDCodiceIVA,
+                                   :FLPrezzoIvato, :PrezzoUnitario, :PrezzoIvato, :LastUpdate)
   end
 end
